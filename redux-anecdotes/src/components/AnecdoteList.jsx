2ffb80167da1bfd7b0342ca294c2897f19a5d795
Santiago Ariel Alvarez <santiiariel2004@gmail.com>
2026-06-17 12:34:04 -0300

    Excersice 6.8

---
+++ redux-anecdotes/src/components/AnecdoteList.jsx
@@ -0,0 +1,31 @@
+import { useSelector, useDispatch } from "react-redux";
+import { voteAnecdote } from "../reducers/anecdoteReducer";
+
+const AnecdoteList = () => {
+  const sortedAnecdotes = (a, b) => {
+    return b.votes - a.votes;
+  };
+
+  const dispatch = useDispatch();
+  const anecdotes = useSelector((state) => state);
+  const anecdotesSorted = anecdotes.sort(sortedAnecdotes);
+
+  const vote = (id) => {
+    dispatch(voteAnecdote(id));
+  };
+  return (
+    <>
+      {anecdotesSorted.map((anecdote) => (
+        <div key={anecdote.id}>
+          <div>{anecdote.content}</div>
+          <div>
+            has {anecdote.votes}
+            <button onClick={() => vote(anecdote.id)}>vote</button>
+          </div>
+        </div>
+      ))}
+    </>
+  );
+};
+
+export default AnecdoteList;
